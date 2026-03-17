12377c0f06c471c95f9cdff6bafa9e889ceadb27
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-03-17 14:41:28 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part3.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
 # hlk7628n dts
 mkdir -p target/linux/ramips/dts/
 cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts

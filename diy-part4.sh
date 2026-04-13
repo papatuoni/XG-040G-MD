5f945f8632c1fa5d0b3f802f3f0947ffde4190c6
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-13 22:30:12 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -3,9 +3,6 @@
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
-# temp
-git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
-git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash

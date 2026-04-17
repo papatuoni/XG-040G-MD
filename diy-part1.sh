11fbfd58c78cdce9b8f9958fe34926c390c64a52
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-17 09:08:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,10 +21,9 @@
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-# hlk7628n dts
-mkdir -p target/linux/ramips/dts/
-cp -f "$GITHUB_WORKSPACE/dts/mt7628an_hilink_hlk-7628n.dts" target/linux/ramips/dts/mt7628an_hilink_hlk-7628n.dts
 
+# OpenClash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
 
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh

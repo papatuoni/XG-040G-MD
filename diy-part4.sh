cf6f52780969facb1f4374424553a3c01fa9409f
papatuoni <105186707+papatuoni@users.noreply.github.com>
2026-06-23 17:42:01 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # turboacc
-curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+#curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # temp
 git clone https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
